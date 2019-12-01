18cd9a7872e47da20fb60e974ee9300bbb8b91c1
Ray Tran <r8tran@gmail.com>
2019-11-30 23:02:00 -0800

    rework day1

--- aoc-2019/day1/day1.py
+++ aoc-2019/day1/day1.py
@@ -1,10 +1,18 @@
 masses = map(int, open('data.txt', 'r').readlines())
 
+# part 1
 def calc_fuel(mass):
-	total, fuel = 0, mass // 3 - 2
-	while fuel > 0:
-		total += fuel 
-		fuel = fuel // 3 - 2
-	return total
+    return mass // 3 - 2
 
 print(sum([calc_fuel(m) for m in masses]))
+
+# part 2
+def calc_additional_fuel(mass):
+    total, fuel = 0, calc_fuel(mass)
+    while fuel > 0:
+        total += fuel
+        fuel = calc_fuel(fuel)
+    return total
+
+print(sum([calc_additional_fuel(m) for m in masses]))
+
