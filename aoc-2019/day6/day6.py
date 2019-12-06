995e66e42fd41d93c7c3d47a74d6ec642896fbfb
Ray Tran <r8tran@gmail.com>
2019-12-05 22:43:14 -0800

    clean up day6

--- aoc-2019/day6/day6.py
+++ aoc-2019/day6/day6.py
@@ -7,18 +7,31 @@ def construct_tree(data):
 		parents[node] = parent
 	return parents
 
-def num_orbits(node, parents, root, memo):
+def num_orbits(node, parent, root, memo):
 	if node == root:
 		return 0
 	if node in memo:
 		return memo[node]
 
 	total = 0
-	p  = parents[node]
-	total = total + num_orbits(p, parents, root, memo) + 1
+	p  = parent[node]
+	total = total + num_orbits(p, parent, root, memo) + 1
 	memo[node] = total
 
 	return total
+
+def get_ancestors(node, parent, root, ancestors):
+	if node == root:
+		return ancestors
+	ancestors.append(parent[node])	
+	return get_ancestors(parent[node], parent, root, ancestors)
+
+def get_common_ancestor(a, b):
+	for i in range(len(a)):
+		for j in range(len(b)):
+			if a[i] == b[j]:
+				return a[i]
+	return None
 
 tree = construct_tree(fileinput.input())
 
@@ -29,22 +42,9 @@ for node in tree:
 print(total)
 
 #part 2
-def get_ancestors(node, parent, ancestors=[]):
-	if node == 'COM':
-		return ancestors
-	ancestors.append(parent[node])	
-	return get_ancestors(parent[node], parent, ancestors)
-
-def get_common_ancestor(a, b):
-	for i in range(len(a)):
-		for j in range(len(b)):
-			if a[i] == b[j]:
-				return a[i]
-	return None
-
 node_a, node_b = tree['YOU'], tree['SAN']
-a = get_ancestors(node_a, tree, [])
-b = get_ancestors(node_b, tree, [])
+a = get_ancestors(node_a, tree, 'COM', [])
+b = get_ancestors(node_b, tree, 'COM', [])
 
 common_ancestor = get_common_ancestor(a, b)
 res1 = num_orbits(node_a, tree, common_ancestor, {})
